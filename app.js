174fab9a2fe5e095ee0b2c9f4843ecdb8de08859
sberzonskiene <s.berzonskiene@gmail.com>
2025-07-30 13:53:12 +0300

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import express from 'express';
 const app = express();
 
@@ -63,13 +64,69 @@ app.get('/api/books/:id', (req,res) => {
 });
 
 // POST
-app.post('/api/books', (req, res) => {
+/*app.post('/api/books', (req, res) => {
     const naujaKnyga = {
         id: books.length + 1,
         author: req.body.author,
         title: req.body.title
     }
     books.push(naujaKnyga);
+    res.send(books);
+});
+
+app.post('/api/books', (req, res) => {
+    if (!req.body.author || !req.body.title || req.body.author.length < 5 || req.body.title.length < 1) {
+        res.status(400).send('autorius turi turėti bent 5 simbolius ir pavadinimas turi turėti bent 1 simbolį.');
+        return;
+    }
+    const naujaKnyga = {
+        id: books.length + 1,
+        author: req.body.author,
+        title: req.body.title
+    }
+    books.push(naujaKnyga);
+    res.send(books);
+});*/
+
+app.post('/api/books', (req, res) => {
+    const schema = Joi.object({
+        author: Joi.string().min(5).required(),
+        title: Joi.string().min(1).required()
+    });
+
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    const naujaKnyga = {
+        id: books.length + 1,
+        author: req.body.author,
+        title: req.body.title
+    }
+    books.push(naujaKnyga);
+    res.send(books);
+});
+
+// PUT / UPDATE
+app.post('/api/books/:id', (req, res) => {
+    const book = books.find(book => book.id === parseInt(req.params.id));
+    if (!book) return res.status(404).send("Book not found ☹️");
+
+    const schema = Joi.object({
+        author: Joi.string().min(5).required(),
+        title: Joi.string().min(1).required()
+    });
+
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    book.author = req.body.author,
+    book.title = req.body.title
     res.send(books);
 });
 
